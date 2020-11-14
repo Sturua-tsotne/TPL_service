cde8f58bcb7f91007c82f0ad993194d5e326e84c
ცოტნე სტურუა <47318145+Sturua-tsotne@users.noreply.github.com>
2020-11-14 20:27:40 +0400

    First migration

---
+++ TplService/Tpl.Api/Migrations/20201114162633_InitialCreate.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tpl.Api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarFeatures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TplModelId = table.Column<int>(type: "int", nullable: false),
+                    RegistrationNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ReleaseTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CarModelId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarFeatures", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarManufacturers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Manufacturer = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarManufacturers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonalInformations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TplModelId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PersonalNumber = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    DateBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MobileNumber = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    IdentityImg = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonalInformations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Model = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CarManufacturerId = table.Column<int>(type: "int", nullable: false),
+                    CarFeatureId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarModels", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CarModels_CarFeatures_CarFeatureId",
+                        column: x => x.CarFeatureId,
+                        principalTable: "CarFeatures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TplModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientId = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    TplImitId = table.Column<int>(type: "int", nullable: false),
+                    CarFeatureId = table.Column<int>(type: "int", nullable: true),
+                    PersonalInformationId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TplModels", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TplModels_CarFeatures_CarFeatureId",
+                        column: x => x.CarFeatureId,
+                        principalTable: "CarFeatures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TplModels_PersonalInformations_PersonalInformationId",
+                        column: x => x.PersonalInformationId,
+                        principalTable: "PersonalInformations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarManufacturerCarModel",
+                columns: table => new
+                {
+                    CarManufacturersId = table.Column<int>(type: "int", nullable: false),
+                    CarModelsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarManufacturerCarModel", x => new { x.CarManufacturersId, x.CarModelsId });
+                    table.ForeignKey(
+                        name: "FK_CarManufacturerCarModel_CarManufacturers_CarManufacturersId",
+                        column: x => x.CarManufacturersId,
+                        principalTable: "CarManufacturers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CarManufacturerCarModel_CarModels_CarModelsId",
+                        column: x => x.CarModelsId,
+                        principalTable: "CarModels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TplLimits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Limit = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Bonus = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    TplModelId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TplLimits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TplLimits_TplModels_TplModelId",
+                        column: x => x.TplModelId,
+                        principalTable: "TplModels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarManufacturerCarModel_CarModelsId",
+                table: "CarManufacturerCarModel",
+                column: "CarModelsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarModels_CarFeatureId",
+                table: "CarModels",
+                column: "CarFeatureId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TplLimits_TplModelId",
+                table: "TplLimits",
+                column: "TplModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TplModels_CarFeatureId",
+                table: "TplModels",
+                column: "CarFeatureId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TplModels_PersonalInformationId",
+                table: "TplModels",
+                column: "PersonalInformationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarManufacturerCarModel");
+
+            migrationBuilder.DropTable(
+                name: "TplLimits");
+
+            migrationBuilder.DropTable(
+                name: "CarManufacturers");
+
+            migrationBuilder.DropTable(
+                name: "CarModels");
+
+            migrationBuilder.DropTable(
+                name: "TplModels");
+
+            migrationBuilder.DropTable(
+                name: "CarFeatures");
+
+            migrationBuilder.DropTable(
+                name: "PersonalInformations");
+        }
+    }
+}
